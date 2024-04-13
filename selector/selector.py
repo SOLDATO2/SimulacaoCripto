dad16761a9a3ecec7943feb2060593d9c4b23318
Fe <felipeporto553@gmail.com>
2024-04-13 15:34:58 -0300

    update receber relogio

--- selector/selector.py
+++ selector/selector.py
@@ -1,13 +1,41 @@
+# selector.py
+
+from flask import Flask, request, jsonify
+import asyncio
+import socket
+import hashlib
+import os
 import requests
+import json
 
-# Definindo o JSON genérico
-json_data = {
-    "key1": "value1",
-    "key2": "value2",
-    "key3": "value3"
-}
+rotas = []
+relogios = []
 
-# Enviando o JSON para a rota do Programa 2
-#Rota esta sendo definida manualmente, tem que ser automatizada
-response = requests.post('http://localhost:5001/receive_json', json=json_data)
-print(response.text)
+app = Flask(__name__)
+
+@app.route('/receber_informacoes', methods=['GET', 'POST'])
+def receber_informacoes():
+    if request.method == 'POST':
+        dados = request.json
+        nova_rota = dados.get('rota')
+        novo_relogio = dados.get('relogio')
+
+        if nova_rota:
+            rotas.append(nova_rota)
+            print(f'Rotas recebida: {rotas}')
+        if novo_relogio:
+            relogios.append(novo_relogio)
+            print(f'Relógios recebidos: {relogios}')
+            
+        return "Dados recebidos com sucesso"
+
+    if len(rotas) == 0 and len(relogios) == 0:
+        print("Aguardando dados...")
+        return "Aguardando dados..."
+    else:
+        print("Rotas recebidas:", rotas)
+        print("Relógios recebidos:", relogios)
+        return jsonify({"rotas": rotas, "relogios": relogios})
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
--- validador/validador.py
+++ validador/validador.py
@@ -1,8 +1,12 @@
-import threading
-from flask import Flask, request
+# validador.py
+
+from flask import Flask, request, jsonify
+import socket
+import hashlib
+import os
+import requests
 import json
 import time
-import socket
 
 def find_available_port():
     """Encontra uma porta disponível."""
@@ -18,17 +22,28 @@ def find_available_port():
             sock.close()
 
 app = Flask(__name__)
-@app.route('/receive_json', methods=['POST'])
-def receive_json():
-    if request.json:
-        json_data = request.json
-        print("JSON received:", json_data)
-        return "JSON received successfully"
-    
-    return "Time exceeded"
+
+def enviar_dados():
+    minha_rota = f'http://localhost:{port}/'  # Obtenha a porta atual do validador.py
+    relogio = time.strftime("%H:%M:%S")  # Obtém o horário atual
+
+    try:
+        response = requests.post('http://localhost:5000/receber_informacoes', json={'rota': minha_rota, 'relogio': relogio})
+        if response.status_code == 200:
+            print("Dados enviados com sucesso para o seletor.")
+            return True
+        else:
+            print("Falha ao enviar dados para o seletor. Tentando novamente...")
+            return False
+    except requests.exceptions.RequestException as e:
+        print("Erro de conexão:", e)
+        return False
 
 if __name__ == '__main__':
     port = find_available_port()
     print(f"Using port: {port}")
+
+    while not enviar_dados():
+        time.sleep(5)  # Espera 5 segundos antes de tentar novamente
+
     app.run(host='localhost', port=port)
-    
