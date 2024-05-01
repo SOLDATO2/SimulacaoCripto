3c702b61c068975edacaab1ccf4827443b8e9c72
Fe <felipeporto553@gmail.com>
2024-04-30 21:32:02 -0300

    update de relogios
    sub-relogio simulador agora é executado no background do validador para simular o relogio de um novo pc.

--- validador/validador.py
+++ validador/validador.py
@@ -1,5 +1,3 @@
-# validador.py
-
 from flask import Flask, request, jsonify
 import socket
 import hashlib
@@ -7,7 +5,7 @@ import os
 import requests
 import json
 import time
-
+import datetime
 import threading
 
 def find_available_port():
@@ -25,35 +23,53 @@ def find_available_port():
 
 app = Flask(__name__)
 
-def enviar_dados():
-    minha_rota = f'http://localhost:{port}/'  # Obtenha a porta atual do validador.py
-    relogio = time.strftime("%H:%M:%S")  # Obtém o horário atual
-    print(relogio)
+def atualizar_relogio():
+    global relogio_atual
+    while True:
+        relogio_atual = datetime.datetime.now().strftime("%H:%M:%S")
+        print(relogio_atual)
+        time.sleep(1)
 
+def enviar_dados():
+    global relogio_atual
+    minha_rota = f'http://localhost:{port}/'  # Obtenha a porta atual do validador.py
+    print("Enviando dados...")
+    
     try:
-        response = requests.post('http://localhost:5000/receber_informacoes', json={'rota': minha_rota, 'relogio': relogio})
+        response = requests.post('http://localhost:5000/receber_informacoes', json={'rota': minha_rota, 'relogio': relogio_atual})
         if response.status_code == 200:
             print("Dados enviados com sucesso para o seletor.")
-            return True
         else:
             print("Falha ao enviar dados para o seletor. Tentando novamente...")
-            return False
     except requests.exceptions.RequestException as e:
         print("Erro de conexão:", e)
-        return False
+    time.sleep(5)
 
 @app.route('/receber_novo_relogio', methods=['POST'])
 def receber_novo_relogio():
+    global relogio_atual
     data = request.json
-    diferenca_relogio = data.get('diferenca_relogio')
-    print("Diferença de relógio recebida:", diferenca_relogio)
-    return "Relógio recebido com sucesso"
+    tempo_recebido = data.get('diferenca_relogio')
+    print("Tempo recebido:", tempo_recebido)
+    sinal = tempo_recebido[0]  # Obtem se eh positivo ou negativo
+    tempo = tempo_recebido[1:]  # Remove o primeiro caractere (sinal)
+    horas, minutos, segundos = map(int, tempo.split(':'))
+    delta = datetime.timedelta(hours=horas, minutes=minutos, seconds=segundos)
+    if sinal == '+':
+        relogio_atual = (datetime.datetime.strptime(relogio_atual, "%H:%M:%S") + delta).strftime("%H:%M:%S")
+    elif sinal == '-':
+        relogio_atual = (datetime.datetime.strptime(relogio_atual, "%H:%M:%S") - delta).strftime("%H:%M:%S")
+    print("Relógio atualizado:", relogio_atual)
+    return "Relógio atualizado com sucesso"
 
 if __name__ == '__main__':
     port = find_available_port()
-    print(f"Using port: {port}")
+    print(f"PORTA: {port}")
 
-    enviar_dados_thread = threading.Thread(target=enviar_dados) #usa multi threading para executar 2 funces ao mesmo tempo
+    thread_relogio = threading.Thread(target=atualizar_relogio)
+    thread_relogio.start()
+
+    enviar_dados_thread = threading.Thread(target=enviar_dados)
     enviar_dados_thread.start()
         
     app.run(host='localhost', port=port)
